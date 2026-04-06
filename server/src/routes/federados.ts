f6f88c189ccf62b94ef23ae22e76fdd8e2dbd1c4
TAMV ONLINE MEXICO <120252809+OsoPanda1@users.noreply.github.com>
2026-04-06 10:48:19 -0600

    feat(federados): add cross-org viable update pipeline for federated repos (#12)

--- server/src/routes/federados.ts
+++ server/src/routes/federados.ts
@@ -4,6 +4,29 @@ import { githubRepoFusionService } from '../services/githubRepoFusionService';
 
 const router = Router();
 
+const DEFAULT_EXTERNAL_FEDERATED_REPOS = [
+  'shreyasnbhat/federated-learning',
+  'mayankshah1607/federated-learning-with-grpc-docker',
+  'jklujklu/Batch-Aggregate',
+  'Victoryao0321/Fedrated-learning',
+  'Breeze1in1drizzle/Fed-Learning-Breeze',
+  'HemanthKumar-CS/Fedrated_DDoS_Detection',
+  'TEOTD/fp-client',
+  'nabilajalil/fedcourse24',
+  'Chukwuemeka-James/Fedrated-Swarm-Behavior',
+  'linhanphan/federated-learning-simulation',
+  'ZAKAUDD/Fedrated-Learning',
+  'dalqattan/HFL-attacks',
+  'Mingyue-Cheng/Awesome-Fedrated-Learning-in-Time-Series',
+  'dprcsingh/fedratedGraph',
+  'gizealew11/FDT_Museum_Evacuation_Simulation',
+  'tianwen1209/FedPTM',
+  'Talal-ALBarqi/decentralized_flower',
+  'Warwick-PDP-Group/FedratedScope',
+  'kanishkdhebana/Fedrated_learning_algorithms',
+  'Tinghao-Chen/FedLPS',
+];
+
 router.get('/overview', (_req, res) => {
   res.json(quantumFederationService.getOverview());
 });
